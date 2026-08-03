7626ee9af6bab99101fd791e8fe4c3266e59c558
M.Syamsandi Yuda Wirawan <89447700+MSyamsandiYudaWirawan@users.noreply.github.com>
2026-08-03 14:42:01 +0700

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        Set<Integer> cur = new LinkedHashSet<>();
+        dfs(nums,cur);
+        return res;
+    }
+    private void dfs(int[] nums,Set<Integer> cur){
+        if(nums.length == cur.size()){
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(cur.contains(nums[i])){
+                continue;
+            }
+            cur.add(nums[i]);
+            dfs(nums,cur);
+            cur.remove(nums[i]);
+        }
+    }
+}
