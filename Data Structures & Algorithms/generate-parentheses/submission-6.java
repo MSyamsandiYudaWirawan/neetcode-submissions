7ed1b0a004599d8caa349985d4a71650ed480eb6
M.Syamsandi Yuda Wirawan <89447700+MSyamsandiYudaWirawan@users.noreply.github.com>
2026-08-03 15:05:55 +0700

    Add: generate-parentheses - submission-6

---
+++ Data Structures & Algorithms/generate-parentheses/submission-6.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        StringBuilder sb = new StringBuilder();
+        dfs(n, 0, 0, sb);
+        return res;
+    }
+    private void dfs(int n, int open, int close, StringBuilder sb) {
+        if (sb.length() == 2 * n) {
+            res.add(sb.toString());
+            return;
+        }
+
+        if (open < n) {
+            sb.append("(");
+            dfs(n, open + 1, close, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if (close < open) {
+            sb.append(")");
+            dfs(n, open, close + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
