6c1f42304c9cb742c51844ca41da4da96964ed43
M.Syamsandi Yuda Wirawan <89447700+MSyamsandiYudaWirawan@users.noreply.github.com>
2026-08-20 16:14:23 +0700

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    Map<Integer, Set<Integer>> preMap = new HashMap<>();
+    Map<Integer, Set<Integer>> nexMap = new HashMap<>();
+    Set<Integer> taken = new HashSet<>();
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for (int i = 0; i < numCourses; i++) {
+            preMap.put(i, new HashSet<>());
+            nexMap.put(i, new HashSet<>());
+        }
+        for (int[] pre : prerequisites) {
+            preMap.get(pre[0]).add(pre[1]);
+            nexMap.get(pre[1]).add(pre[0]);
+        }
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (preMap.get(i).isEmpty()) q.add(i);
+        }
+        
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (taken.contains(cur)) continue;
+            taken.add(cur);
+            
+            for (int nex : nexMap.get(cur)) {
+                preMap.get(nex).remove((Integer) cur); // cur is now satisfied
+                if (preMap.get(nex).isEmpty()) {
+                    q.add(nex);
+                }
+            }
+        }
+        return taken.size() == numCourses;
+    }
+}
