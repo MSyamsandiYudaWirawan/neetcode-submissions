223e709c2f1584a2c73afc4c71a145ea7f5989a4
M.Syamsandi Yuda Wirawan <89447700+MSyamsandiYudaWirawan@users.noreply.github.com>
2026-08-21 09:48:45 +0700

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.java
@@ -0,0 +1,47 @@
+class Solution {
+    Map<Integer, Integer> par = new HashMap<>();
+    Map<Integer, Integer> rank = new HashMap<>();
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length + 1;
+        for (int i = 0; i < n; i++) {
+            par.put(i, i);
+            rank.put(i, 0);
+        }
+
+        for (int[] edge : edges) {
+            if (!union(edge[0], edge[1])) {
+                return new int[] {edge[0], edge[1]};
+            }
+        }
+        return new int[] {};
+    }
+    private int find(int n) {
+        int p = par.get(n);
+        while (p != par.get(p)) {
+            par.put(p, par.get(par.get(p)));
+            p = par.get(p);
+        }
+        return p;
+    }
+    private boolean union(int n1, int n2) {
+        int p1 = find(n1);
+        int p2 = find(n2);
+
+        if (p1 == p2) {
+            return false;
+        }
+        if (rank.get(p1) > rank.get(p2)) {
+            // merge p2 to root p1
+            par.put(p2, p1);
+            // update rank p1 = p1 + p2
+        } else if (rank.get(p1) < rank.get(p2)) {
+            // same operation jsut reverse
+            par.put(p1, p2);
+        }else{
+            par.put(p1,p2);
+            rank.put(p2, rank.get(p2) + 1);
+        }
+        return true;
+    }
+}
