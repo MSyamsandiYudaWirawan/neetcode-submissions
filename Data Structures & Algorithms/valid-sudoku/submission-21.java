7b327ea672fa589f97dc7e4c80b660d5b1559c41
M.Syamsandi Yuda Wirawan <89447700+MSyamsandiYudaWirawan@users.noreply.github.com>
2026-09-07 06:21:28 +0700

    Add: valid-sudoku - submission-21

---
+++ Data Structures & Algorithms/valid-sudoku/submission-21.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        List<Set<Integer>> rows = new ArrayList<>();
+        List<Set<Integer>> cols = new ArrayList<>();
+        List<Set<Integer>> boxes = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            rows.add(new HashSet<>());
+            cols.add(new HashSet<>());
+            boxes.add(new HashSet<>());
+        }
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                char c = board[row][col];
+                if (c == '.'){
+                    continue;
+                }
+                int num = c - '0';
+
+                int boxidx = (row/3)*3 + (col/3);
+
+                if(rows.get(row).contains(num) || cols.get(col).contains(num) || boxes.get(boxidx).contains(num)){
+                    return false;
+                }
+                rows.get(row).add(num);
+                cols.get(col).add(num);
+                boxes.get(boxidx).add(num);
+            }
+        }
+        return true;
+    }
+}
