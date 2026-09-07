78bd432ebe295ec46bf2e66b0a25b56cb53742c4
M.Syamsandi Yuda Wirawan <89447700+MSyamsandiYudaWirawan@users.noreply.github.com>
2026-09-07 07:52:51 +0700

    Add: count-number-of-islands - submission-9

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-9.java
@@ -0,0 +1,45 @@
+class Solution {
+    Pair[] directions = {
+        new Pair(0, 1),
+        new Pair(0, -1),
+        new Pair(1, 0),
+        new Pair(-1, 0),
+    };
+    int ROWS;
+    int COLS;
+    private record Pair(int r, int c) {}
+    public int numIslands(char[][] grid) {
+        ROWS = grid.length;
+        COLS = grid[0].length;
+        int res = 0;
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (grid[r][c] == '1') {
+                    bfs(grid, r, c);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+    private void bfs(char[][] grid, int r, int c) {
+        Queue<Pair> q = new ArrayDeque();
+        q.add(new Pair(r, c));
+        grid[r][c] = '0';
+
+        while (!q.isEmpty()) {
+            Pair cur = q.poll();
+
+            for (Pair dir : directions) {
+                int nr = cur.r + dir.r;
+                int nc = cur.c + dir.c;
+
+                if (nr >= 0 && nc >= 0 && nr < ROWS && nc < COLS && grid[nr][nc] == '1') {
+                    grid[nr][nc] = '0';
+                    q.add(new Pair(nr,nc));
+                }
+            }
+        }
+    }
+}
