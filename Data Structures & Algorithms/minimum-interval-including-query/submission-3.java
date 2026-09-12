faf633f2a1ef0dc2f0b976bf02b16791446997d2
M.Syamsandi Yuda Wirawan <89447700+MSyamsandiYudaWirawan@users.noreply.github.com>
2026-09-12 07:42:29 +0700

    Add: minimum-interval-including-query - submission-3

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-3.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] minInterval(int[][] intervals, int[] queries) {
+        record Inter(int len, int right) {}
+        record Query(int idx, int val) {}
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<Query> qs = new ArrayList<>();
+        PriorityQueue<Inter> pq = new PriorityQueue<>((a, b) -> a.len - b.len);
+
+        for (int i = 0; i < queries.length; i++) {
+            qs.add(new Query(i, queries[i]));
+        }
+        qs.sort((a, b) -> a.val - b.val);
+
+        int i = 0;
+        int[] res = new int[queries.length];
+
+        for (Query q : qs) {
+            int idx = q.idx;
+            int val = q.val;
+
+            while (i < intervals.length && intervals[i][0] <= val) {
+                pq.add(new Inter(intervals[i][1] - intervals[i][0] + 1, intervals[i][1]));
+                i++;
+            }
+            while (!pq.isEmpty() && pq.peek().right < val) {
+                pq.poll();
+            }
+            res[idx] = !pq.isEmpty() ? pq.peek().len : -1;
+        }
+        return res;
+    }
+}
